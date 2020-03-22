dc73eab34700bf175111e153d4d665d840b2f10e
Grace Berman <grace.g.berman@gmail.com>
2020-03-22 16:50:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,6 +30,46 @@ WIN_COMBINATIONS = [
   [2,4,6]   # Right diagonal
 ]
 
+def valid_move?(board, index)
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip.to_i
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 # board = ["X", " ", " ", "X", " ", " ", "X", " ", " "]
 def won?(board)
   won_combination = false
@@ -68,4 +108,19 @@ def winner(board)
      end
   end
   winner
+end
+
+def play(board)
+
+  spaces_available = board.size
+  spaces_to_start = 0
+
+  loop do
+    if spaces_to_start < spaces_available
+      turn(board)
+      spaces_to_start += 1
+    else
+      break
+end
+end
 end
